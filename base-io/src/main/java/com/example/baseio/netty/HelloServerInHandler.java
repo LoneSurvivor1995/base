bfe21bda41416715e88213fd0f46bedca512560f
LoneSurvivor1995 <412018423@qq.com>
2018-08-29 18:15:19 +0800

    demo

---
+++ base-io/src/main/java/com/example/baseio/netty/HelloServerInHandler.java
@@ -0,0 +1,15 @@
+package com.example.baseio.netty;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloServerInHandler extends ChannelInboundHandlerAdapter {
+    private static Logger logger = LoggerFactory.getLogger(HelloServerInHandler.class);
+
+
+}
+
